b2e57c14ad2a62754bdbc0fb99c1877b1e64b0e4
Ionut Pasat <ionut_vladut.pasat@stud.acs.upb.ro>
2025-03-24 18:18:55 +0200

    added array sum method

---
+++ MyAlgorithm.java
@@ -0,0 +1,17 @@
+// write a java method that takes an array of integers and returns the sum of the array
+// Example: [1, 2, 3, 4, 5] -> 15
+
+public class MyAlgorithm {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println(sumArray(arr));
+    }
+
+    public static int sumArray(int[] arr) {
+        int sum = 0;
+        for (int j : arr) {
+            sum += j;
+        }
+        return sum;
+    }
+}
